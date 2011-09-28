8520ea416d3556bc651d9bba15fba59896f3a4d7
Andres Soolo <dig@mirky.net>
2011-09-29 02:26:59 +0300

    created Saguaro.java as a module and moved ZXSBasicProgramCodeAnalyser.SaguaroLineLexer -> Saguaro.LineLexer and ZXSBasicProgramCodeAnalyser.loadStringTableAsArray() -> Saguaro.loadStringTableAsArray()

---
+++ src/net/mirky/redis/Saguaro.java
@@ -0,0 +1,122 @@
+package net.mirky.redis;
+
+public final class Saguaro {
+    private Saguaro() {
+        // not a real constructor
+    }
+
+    public static final class LineLexer {
+        private final String line;
+        private int pos;
+    
+        public LineLexer(String line) {
+            this.line = line;
+            pos = 0;
+        }
+        
+        public final void skipSpaces() {
+            while (pos < line.length() && line.charAt(pos) == ' ') {
+                pos++;
+            }
+        }
+        
+        public final void skipChar() {
+            if (pos < line.length()) {
+                pos++;
+            }
+        }
+        
+        public final boolean atUnsignedInteger() {
+            if (pos >= line.length()) {
+                return false;
+            }
+            char c = line.charAt(pos);
+            return c >= '0' && c <= '9';
+        }
+        
+        public final boolean atString() {
+            return at('"');
+        }
+    
+        public final String parseString() {
+            assert atString();
+            int begin = pos + 1;
+            for (int cur = begin; cur < line.length(); cur++) {
+                if (line.charAt(cur) == '"') {
+                    pos = cur + 1;
+                    return line.substring(begin, cur);
+                }
+            }
+            throw new RuntimeException("string not terminated");
+        }
+        
+        public final boolean at(char etalon) {
+            return pos < line.length() && line.charAt(pos) == etalon;
+        }
+        
+        public final boolean atEndOfLine() {
+            return pos >= line.length();
+        }
+    
+        public final String parseWord() {
+            int begin = pos;
+            while (pos < line.length() && isWordChar(line.charAt(pos))) {
+                pos++;
+            }
+            return line.substring(begin, pos);
+        }
+    
+        public final int parseUnsignedInteger() throws NumberFormatException {
+            assert atUnsignedInteger();
+            String word = parseWord().toLowerCase();
+            if (word.length() >= 3 && word.startsWith("0x")) {
+                return Integer.parseInt(word.substring(2), 16);
+            } else {
+                return Integer.parseInt(word);
+            }
+        }
+    
+        public static final boolean isWordChar(char c) {
+            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+        }
+    }
+
+    // XXX: we're currently fully ignoring any possible indentation
+    // XXX: and we're not ignoring empty lines and comments, either
+    public static final String[] loadStringTableAsArray(String filename, int arraySize) throws NumberFormatException {
+        String[] keywords = new String[arraySize];
+        for (int i = 0; i < keywords.length; i++) {
+            keywords[i] = null;
+        }
+        for (String line : new TextResource(filename)) {
+            Saguaro.LineLexer lexer = new LineLexer(line);
+            lexer.skipSpaces();
+            if (!lexer.atUnsignedInteger()) {
+                throw new RuntimeException("invalid " + filename + " line: " + line);
+            }
+            int key = lexer.parseUnsignedInteger();
+            lexer.skipSpaces();
+            if (!lexer.at(':')) {
+                throw new RuntimeException("invalid " + filename + " line: " + line);
+            }
+            lexer.skipChar();
+            lexer.skipSpaces();
+            if (!lexer.atString()) {
+                throw new RuntimeException("invalid " + filename + " line: " + line);
+            }
+            String value = lexer.parseString();
+            lexer.skipSpaces();
+            if (!lexer.atEndOfLine()) {
+                throw new RuntimeException("invalid " + filename + " line: " + line);
+            }
+            if (key < 0 || key >= arraySize) {
+                throw new RuntimeException("key " + key + " out of bounds in " + filename);
+            }
+            if (keywords[key] != null) {
+                throw new RuntimeException("duplicate " + filename + " entry for " + key);
+            }
+            keywords[key] = value;
+        }
+        return keywords;
+    }
+}
--- src/net/mirky/redis/analysers/ZXSBasicProgramCodeAnalyser.java
+++ src/net/mirky/redis/analysers/ZXSBasicProgramCodeAnalyser.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 
+import net.mirky.redis.Analyser;
 import net.mirky.redis.BichromaticStringBuilder;
-import net.mirky.redis.BichromaticStringBuilder.DelimitedMode;
 import net.mirky.redis.Cursor;
 import net.mirky.redis.Format;
-import net.mirky.redis.Analyser;
 import net.mirky.redis.Hex;
 import net.mirky.redis.ImageError;
-import net.mirky.redis.TextResource;
+import net.mirky.redis.Saguaro;
 
 @Format.Options("zxs-basic-code/decoding:decoding=zx-spectrum/autostart:unsigned-decimal=0")
 public final class ZXSBasicProgramCodeAnalyser extends Analyser.Leaf.PossiblyPartial {
@@ -32,7 +31,7 @@ public final class ZXSBasicProgramCodeAnalyser extends Analyser.Leaf.PossiblyPar
                  * confusiont that might raise if we used, say, ASCII brokets.
                  */
                 BichromaticStringBuilder bsb = new BichromaticStringBuilder("33;1");
-                DelimitedMode del = new BichromaticStringBuilder.DelimitedMode(bsb, '`', ' ', '`');
+                BichromaticStringBuilder.DelimitedMode del = new BichromaticStringBuilder.DelimitedMode(bsb, '`', ' ', '`');
                 int lineNumber = cursor.getUnsignedBewyde(0); // sic, line number is big endian
                 int lineSize = cursor.getUnsignedLewyde(2);
                 if (!cursor.probe(4 + lineSize)) {
@@ -172,118 +171,5 @@ public final class ZXSBasicProgramCodeAnalyser extends Analyser.Leaf.PossiblyPar
         }
     }
 
-    private static final String[] KEYWORDS = ZXSBasicProgramCodeAnalyser.loadStringTableAsArray("zxsbaskw.tab", 256);
-
-    private static final class SaguaroLineLexer {
-        private final String line;
-        private int pos;
-
-        public SaguaroLineLexer(String line) {
-            this.line = line;
-            pos = 0;
-        }
-        
-        public final void skipSpaces() {
-            while (pos < line.length() && line.charAt(pos) == ' ') {
-                pos++;
-            }
-        }
-        
-        public final void skipChar() {
-            if (pos < line.length()) {
-                pos++;
-            }
-        }
-        
-        public final boolean atUnsignedInteger() {
-            if (pos >= line.length()) {
-                return false;
-            }
-            char c = line.charAt(pos);
-            return c >= '0' && c <= '9';
-        }
-        
-        public final boolean atString() {
-            return at('"');
-        }
-
-        public final String parseString() {
-            assert atString();
-            int begin = pos + 1;
-            for (int cur = begin; cur < line.length(); cur++) {
-                if (line.charAt(cur) == '"') {
-                    pos = cur + 1;
-                    return line.substring(begin, cur);
-                }
-            }
-            throw new RuntimeException("string not terminated");
-        }
-        
-        public final boolean at(char etalon) {
-            return pos < line.length() && line.charAt(pos) == etalon;
-        }
-        
-        public final boolean atEndOfLine() {
-            return pos >= line.length();
-        }
-
-        private final String parseWord() {
-            int begin = pos;
-            while (pos < line.length() && isWordChar(line.charAt(pos))) {
-                pos++;
-            }
-            return line.substring(begin, pos);
-        }
-
-        public final int parseUnsignedInteger() throws NumberFormatException {
-            assert atUnsignedInteger();
-            String word = parseWord().toLowerCase();
-            if (word.length() >= 3 && word.startsWith("0x")) {
-                return Integer.parseInt(word.substring(2), 16);
-            } else {
-                return Integer.parseInt(word);
-            }
-        }
-
-        private static final boolean isWordChar(char c) {
-            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
-        }
-    }
-
-    private static final String[] loadStringTableAsArray(String filename, int arraySize) throws NumberFormatException {
-        String[] keywords = new String[arraySize];
-        for (int i = 0; i < keywords.length; i++) {
-            keywords[i] = null;
-        }
-        for (String line : new TextResource(filename)) {
-            SaguaroLineLexer lexer = new SaguaroLineLexer(line);
-            lexer.skipSpaces();
-            if (!lexer.atUnsignedInteger()) {
-                throw new RuntimeException("invalid " + filename + " line: " + line);
-            }
-            int key = lexer.parseUnsignedInteger();
-            lexer.skipSpaces();
-            if (!lexer.at(':')) {
-                throw new RuntimeException("invalid " + filename + " line: " + line);
-            }
-            lexer.skipChar();
-            lexer.skipSpaces();
-            if (!lexer.atString()) {
-                throw new RuntimeException("invalid " + filename + " line: " + line);
-            }
-            String value = lexer.parseString();
-            lexer.skipSpaces();
-            if (!lexer.atEndOfLine()) {
-                throw new RuntimeException("invalid " + filename + " line: " + line);
-            }
-            if (key < 0 || key >= arraySize) {
-                throw new RuntimeException("key " + key + " out of bounds in " + filename);
-            }
-            if (keywords[key] != null) {
-                throw new RuntimeException("duplicate " + filename + " entry for " + key);
-            }
-            keywords[key] = value;
-        }
-        return keywords;
-    }
+    private static final String[] KEYWORDS = Saguaro.loadStringTableAsArray("zxsbaskw.tab", 256);
 }
