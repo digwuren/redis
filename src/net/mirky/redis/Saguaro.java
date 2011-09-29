21e9dc66aa5b28f5a9305dbfa82371fd85cddabb
Andres Soolo <dig@mirky.net>
2011-09-29 18:40:43 +0300

    introduced Saguaro.LineParseError (currently not thrown out of any function)

--- src/net/mirky/redis/Saguaro.java
+++ src/net/mirky/redis/Saguaro.java
@@ -97,6 +97,15 @@ public final class Saguaro {
             return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
         }
     }
+    
+    public static final class LineParseError extends Exception {
+        public final String line;
+
+        public LineParseError(String msg, String line) {
+            super(msg);
+            this.line = line;
+        }
+    }
 
     // XXX: we're currently fully ignoring any possible indentation
     // XXX: and we're not ignoring empty lines and comments, either
@@ -106,29 +115,29 @@ public final class Saguaro {
             keywords[i] = null;
         }
         BufferedReader reader = TextResource.getBufferedReader(filename);
-        String line;
         try {
             try {
+                String line;
                 while ((line = reader.readLine()) != null) {
                     Saguaro.LineLexer lexer = new LineLexer(line);
                     lexer.skipSpaces();
                     if (!lexer.atUnsignedInteger()) {
-                        throw new RuntimeException("invalid " + filename + " line: " + line);
+                        throw new LineParseError("wrong key type", line);
                     }
                     int key = lexer.parseUnsignedInteger();
                     lexer.skipSpaces();
                     if (!lexer.at(':')) {
-                        throw new RuntimeException("invalid " + filename + " line: " + line);
+                        throw new LineParseError("missing colon after key", line);
                     }
                     lexer.skipChar();
                     lexer.skipSpaces();
                     if (!lexer.atString()) {
-                        throw new RuntimeException("invalid " + filename + " line: " + line);
+                        throw new LineParseError("wrong value type", line);
                     }
                     String value = lexer.parseString();
                     lexer.skipSpaces();
                     if (!lexer.atEndOfLine()) {
-                        throw new RuntimeException("invalid " + filename + " line: " + line);
+                        throw new LineParseError("missing end of line", line);
                     }
                     if (key < 0 || key >= arraySize) {
                         throw new RuntimeException("key " + key + " out of bounds in " + filename);
@@ -143,6 +152,8 @@ public final class Saguaro {
             }
         } catch (IOException e) {
             throw new RuntimeException("I/O error reading resource " + filename, e);
+        } catch (LineParseError e) {
+            throw new RuntimeException("invalid " + filename + " line: " + e.line, e);
         }
         return keywords;
     }
