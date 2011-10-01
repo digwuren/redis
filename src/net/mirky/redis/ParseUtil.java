debadef3020df46510ecc0bcb7c4997f94ac025f
Andres Soolo <dig@mirky.net>
2011-10-01 16:52:19 +0300

    documented ParseUtil.parseUnsignedInteger()

--- src/net/mirky/redis/ParseUtil.java
+++ src/net/mirky/redis/ParseUtil.java
@@ -5,6 +5,17 @@ public final class ParseUtil {
         // not a real constructor
     }
 
+    /**
+     * Parses a given string as an unsigned integer; decimal if unprefixed,
+     * hexadecimal if with 0x prefix.
+     * 
+     * @param s
+     *            pretrimmed string to be parsed
+     * @return resulting integer
+     * @throws NumberFormatException
+     *             if the input is not a valid integer or does not fit into
+     *             Java's {@code int}
+     */
     public static final int parseUnsignedInteger(String s) throws NumberFormatException {
         if (s.length() >= 3 && s.toLowerCase().startsWith("0x")) {
             return Integer.parseInt(s.substring(2), 16);
