575913078b2fe809ebf76316ab0e6d555fde7d43
Andres Soolo <dig@mirky.net>
2011-10-01 17:05:41 +0300

    moved Saguaro.LineLexer to ParseUtil.LineLexer

--- src/net/mirky/redis/ParseUtil.java
+++ src/net/mirky/redis/ParseUtil.java
@@ -23,4 +23,86 @@ public final class ParseUtil {
             return Integer.parseInt(s);
         }
     }
+
+    public static final class LineLexer {
+        private final String line;
+        private int pos;
+    
+        public LineLexer(String line) {
+            this.line = line;
+            pos = 0;
+        }
+        
+        public final void skipSpaces() {
+            while (pos < line.length() && line.charAt(pos) == ' ') {
+                pos++;
+            }
+        }
+        
+        public final void skipChar() {
+            if (pos < line.length()) {
+                pos++;
+            }
+        }
+        
+        public final boolean atUnsignedInteger() {
+            if (pos >= line.length()) {
+                return false;
+            }
+            char c = line.charAt(pos);
+            return c >= '0' && c <= '9';
+        }
+        
+        public final boolean atString() {
+            return at('"');
+        }
+    
+        public final String parseString() {
+            assert atString();
+            StringBuilder sb = new StringBuilder();
+            for (int cur = pos + 1; cur < line.length(); cur++) {
+                if (line.charAt(cur) == '"') {
+                    pos = cur + 1;
+                    return sb.toString();
+                }
+                if (line.charAt(cur) == '\\') {
+                    cur++;
+                    if (cur >= line.length()) {
+                        break; // and complain about termination
+                    }
+                    char c = line.charAt(cur);
+                    // XXX: currently, \ is a pure escape character; there are no specials
+                    sb.append(c);
+                } else {
+                    sb.append(line.charAt(cur));
+                }
+            }
+            throw new RuntimeException("string not terminated");
+        }
+        
+        public final boolean at(char etalon) {
+            return pos < line.length() && line.charAt(pos) == etalon;
+        }
+        
+        public final boolean atEndOfLine() {
+            return pos >= line.length();
+        }
+    
+        public final String parseWord() {
+            int begin = pos;
+            while (pos < line.length() && isWordChar(line.charAt(pos))) {
+                pos++;
+            }
+            return line.substring(begin, pos);
+        }
+    
+        public final int parseUnsignedInteger() throws NumberFormatException {
+            assert atUnsignedInteger();
+            return ParseUtil.parseUnsignedInteger(parseWord());
+        }
+    
+        public static final boolean isWordChar(char c) {
+            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+        }
+    }
 }
--- src/net/mirky/redis/Saguaro.java
+++ src/net/mirky/redis/Saguaro.java
@@ -11,88 +11,6 @@ public final class Saguaro {
         // not a real constructor
     }
 
-    public static final class LineLexer {
-        private final String line;
-        private int pos;
-    
-        public LineLexer(String line) {
-            this.line = line;
-            pos = 0;
-        }
-        
-        public final void skipSpaces() {
-            while (pos < line.length() && line.charAt(pos) == ' ') {
-                pos++;
-            }
-        }
-        
-        public final void skipChar() {
-            if (pos < line.length()) {
-                pos++;
-            }
-        }
-        
-        public final boolean atUnsignedInteger() {
-            if (pos >= line.length()) {
-                return false;
-            }
-            char c = line.charAt(pos);
-            return c >= '0' && c <= '9';
-        }
-        
-        public final boolean atString() {
-            return at('"');
-        }
-    
-        public final String parseString() {
-            assert atString();
-            StringBuilder sb = new StringBuilder();
-            for (int cur = pos + 1; cur < line.length(); cur++) {
-                if (line.charAt(cur) == '"') {
-                    pos = cur + 1;
-                    return sb.toString();
-                }
-                if (line.charAt(cur) == '\\') {
-                    cur++;
-                    if (cur >= line.length()) {
-                        break; // and complain about termination
-                    }
-                    char c = line.charAt(cur);
-                    // XXX: currently, \ is a pure escape character; there are no specials
-                    sb.append(c);
-                } else {
-                    sb.append(line.charAt(cur));
-                }
-            }
-            throw new RuntimeException("string not terminated");
-        }
-        
-        public final boolean at(char etalon) {
-            return pos < line.length() && line.charAt(pos) == etalon;
-        }
-        
-        public final boolean atEndOfLine() {
-            return pos >= line.length();
-        }
-    
-        public final String parseWord() {
-            int begin = pos;
-            while (pos < line.length() && isWordChar(line.charAt(pos))) {
-                pos++;
-            }
-            return line.substring(begin, pos);
-        }
-    
-        public final int parseUnsignedInteger() throws NumberFormatException {
-            assert atUnsignedInteger();
-            return ParseUtil.parseUnsignedInteger(parseWord());
-        }
-
-        public static final boolean isWordChar(char c) {
-            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
-        }
-    }
-    
     public static final class LineParseError extends Exception {
         public final String line;
 
@@ -114,7 +32,7 @@ public final class Saguaro {
             try {
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    Saguaro.LineLexer lexer = new LineLexer(line);
+                    ParseUtil.LineLexer lexer = new ParseUtil.LineLexer(line);
                     lexer.skipSpaces();
                     if (!lexer.atUnsignedInteger()) {
                         throw new LineParseError("wrong key type", line);
@@ -162,7 +80,7 @@ public final class Saguaro {
         try {
             try {
                 while ((line = reader.readLine()) != null) {
-                    Saguaro.LineLexer lexer = new LineLexer(line);
+                    ParseUtil.LineLexer lexer = new ParseUtil.LineLexer(line);
                     lexer.skipSpaces();
                     if (!lexer.atString()) {
                         throw new RuntimeException("invalid " + filename + " line: " + line);
