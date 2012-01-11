af0aa34d9a27d3f4847363451cffbc2a01ef0c0c
Andres Soolo <dig@mirky.net>
2012-01-11 05:59:41 +0200

    introduced IndentationSensitiveLexer

--- src/net/mirky/redis/ParseUtil.java
+++ src/net/mirky/redis/ParseUtil.java
@@ -1,5 +1,11 @@
 package net.mirky.redis;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Vector;
+
+import net.mirky.redis.ControlData.LineParseError;
+
 public final class ParseUtil {
     private ParseUtil() {
         // not a real constructor
@@ -25,26 +31,26 @@ public final class ParseUtil {
     }
 
     public static final class LineLexer {
-        private final String line;
+        public final String line;
         private int pos;
-    
+
         public LineLexer(String line) {
             this.line = line;
             pos = 0;
         }
-        
+
         public final void skipSpaces() {
             while (pos < line.length() && line.charAt(pos) == ' ') {
                 pos++;
             }
         }
-        
+
         public final void skipChar() {
             if (pos < line.length()) {
                 pos++;
             }
         }
-        
+
         public final boolean atUnsignedInteger() {
             if (pos >= line.length()) {
                 return false;
@@ -52,11 +58,11 @@ public final class ParseUtil {
             char c = line.charAt(pos);
             return c >= '0' && c <= '9';
         }
-        
+
         public final boolean atString() {
             return at('"');
         }
-    
+
         public final String parseString() {
             assert atString();
             StringBuilder sb = new StringBuilder();
@@ -71,7 +77,8 @@ public final class ParseUtil {
                         break; // and complain about termination
                     }
                     char c = line.charAt(cur);
-                    // XXX: currently, \ is a pure escape character; there are no specials
+                    // XXX: currently, \ is a pure escape character; there are
+                    // no specials
                     sb.append(c);
                 } else {
                     sb.append(line.charAt(cur));
@@ -79,15 +86,15 @@ public final class ParseUtil {
             }
             throw new RuntimeException("string not terminated");
         }
-        
+
         public final boolean at(char etalon) {
             return pos < line.length() && line.charAt(pos) == etalon;
         }
-        
+
         public final boolean atEndOfLine() {
             return pos >= line.length();
         }
-    
+
         public final String parseWord() {
             int begin = pos;
             while (pos < line.length() && isWordChar(line.charAt(pos))) {
@@ -95,14 +102,210 @@ public final class ParseUtil {
             }
             return line.substring(begin, pos);
         }
-    
+
         public final int parseUnsignedInteger() throws NumberFormatException {
             assert atUnsignedInteger();
             return ParseUtil.parseUnsignedInteger(parseWord());
         }
-    
+
+        public final int getPos() {
+            return pos;
+        }
+
         public static final boolean isWordChar(char c) {
             return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+        }
+    }
+
+    public static abstract class IndentationSensitiveLexer {
+        private LineLexer lineLexer = null;
+        private boolean eof = false;
+        private final Vector<Integer> indentationStack = new Vector<Integer>();
+        private final char commentChar;
+        private int dent; // +1 for indent, negative for dedent, the absolute
+                          // value indicates the count of remaining dedents
+
+        public IndentationSensitiveLexer(char commentChar) {
+            this.commentChar = commentChar;
+        }
+
+        public final void advanceVertically() throws LineParseError, IOException {
+            do {
+                String line = getNextLine();
+                if (line == null) {
+                    lineLexer = null;
+                    eof = true;
+                    dent = -indentationStack.size();
+                    indentationStack.clear();
+                    return;
+                }
+                lineLexer = new LineLexer(line);
+                lineLexer.skipSpaces();
+            } while (lineLexer.atEndOfLine() || lineLexer.at(commentChar));
+            int threshold = indentationStack.isEmpty() ? 0 : indentationStack.get(indentationStack.size() - 1)
+                    .intValue();
+            if (lineLexer.getPos() > threshold) {
+                dent = 1;
+                indentationStack.add(new Integer(lineLexer.getPos()));
+            } else {
+                dent = 0;
+                while (lineLexer.getPos() < threshold) {
+                    dent--;
+                    indentationStack.remove(indentationStack.size() - 1);
+                    threshold = indentationStack.isEmpty() ? 0 : indentationStack.get(indentationStack.size() - 1)
+                            .intValue();
+                }
+                if (lineLexer.getPos() != threshold) {
+                    complain("invalid dedent");
+                }
+            }
+        }
+
+        /**
+         * Check whether we're at the end of file. Note that this means having
+         * actually advanced vertically past the final interesting line in this
+         * file *and* having zero dent. Standing at the end of the last line
+         * does not count as standing at the end of the file.
+         * 
+         * @throws IOException
+         * @throws LineParseError
+         */
+        public final boolean atEndOfFile() throws LineParseError, IOException {
+            ensureCurrentLine();
+            return dent == 0 && eof;
+        }
+
+        /**
+         * Check whether we're at the end of a physical line.
+         * 
+         * @throws IOException
+         * @throws LineParseError
+         */
+        public final boolean atEndOfLine() throws LineParseError, IOException {
+            ensureCurrentLine();
+            return dent == 0 && (eof || lineLexer.atEndOfLine());
+        }
+
+        public final boolean atIndent() {
+            return dent > 0;
+        }
+
+        public final boolean atDedent() {
+            return dent < 0;
+        }
+
+        public final boolean at(char c) throws LineParseError, IOException {
+            ensureCurrentLine();
+            return dent == 0 && !eof && lineLexer.at(c);
+        }
+
+        public final void skipIndent() {
+            assert atIndent();
+            dent--;
+        }
+
+        public final void skipDedent() {
+            assert atDedent();
+            dent++;
+        }
+
+        public final void skipChar() throws LineParseError, IOException {
+            ensureCurrentLine();
+            assert dent == 0 && !eof;
+            lineLexer.skipChar();
+        }
+
+        public final boolean atUnsignedInteger() throws LineParseError, IOException {
+            ensureCurrentLine();
+            return dent == 0 && !eof && lineLexer.atUnsignedInteger();
+        }
+
+        public final int parseUnsignedInteger() throws LineParseError, IOException {
+            ensureCurrentLine();
+            assert dent == 0 && !eof;
+            return lineLexer.parseUnsignedInteger();
+        }
+
+        /**
+         * Skip horizontal whitespace. If there's any unprocessing indent or
+         * dedent, it will be cleared.
+         * 
+         * @throws IOException
+         * @throws LineParseError
+         */
+        public final void skipSpaces() throws LineParseError, IOException {
+            ensureCurrentLine();
+            dent = 0;
+            if (!eof) {
+                lineLexer.skipSpaces();
+            }
+        }
+
+        public final boolean atString() throws LineParseError, IOException {
+            ensureCurrentLine();
+            return dent == 0 && !eof && lineLexer.atString();
+        }
+
+        public final String parseString() throws LineParseError, IOException {
+            ensureCurrentLine();
+            assert dent == 0 && !eof;
+            return lineLexer.parseString();
+        }
+
+        public final boolean atCommentChar() throws LineParseError, IOException {
+            ensureCurrentLine();
+            return at(commentChar);
+        }
+        
+        private final void ensureCurrentLine() throws LineParseError, IOException {
+            if (lineLexer == null && !eof) {
+                advanceVertically();
+            }
+        }
+
+        /**
+         * Fetch the next line and return it as a {@link String} instance or
+         * return {@code null} if the line source has ended.
+         * 
+         * @throws IOException
+         */
+        protected abstract String getNextLine() throws IOException;
+
+        /**
+         * Return the location marker for the currently loaded line, preferably
+         * in the standard filename:lineno form. Used for error reporting.
+         */
+        protected abstract String getLineLoc();
+
+        public final void complain(String message) throws LineParseError {
+            if (!eof) {
+                throw new LineParseError(getLineLoc() + ':' + (lineLexer.getPos() + 1) + ": " + message, lineLexer.line);
+            } else {
+                throw new LineParseError(getLineLoc() + ": " + message, "");
+            }
+        }
+    }
+
+    public static final class IndentationSensitiveFileLexer extends IndentationSensitiveLexer {
+        private final BufferedReader reader;
+        private final String filename;
+        private int lineno = 0;
+
+        public IndentationSensitiveFileLexer(BufferedReader reader, String filename, char commentChar) {
+            super(commentChar);
+            this.reader = reader;
+            this.filename = filename;
+        }
+
+        @Override
+        protected final String getNextLine() throws IOException {
+            lineno++;
+            return reader.readLine();
+        }
+
+        @Override
+        protected final String getLineLoc() {
+            return filename + ':' + lineno;
         }
     }
 }
