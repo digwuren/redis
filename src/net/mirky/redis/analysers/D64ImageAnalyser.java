f597d9533c644683f1b2c3daa99b87b8c30a50d5
Andres Soolo <dig@mirky.net>
2012-01-29 22:00:51 +0200

    deduce the step automatically in StructArrayAnalyser

--- src/net/mirky/redis/analysers/D64ImageAnalyser.java
+++ src/net/mirky/redis/analysers/D64ImageAnalyser.java
@@ -339,7 +339,7 @@ public final class D64ImageAnalyser extends Analyser.Container {
                 }
                 Format dirFormat = Format.getGuaranteedFormat("array");
                 try {
-                    dirFormat = dirFormat.parseChange("/struct=d64-dirent-union/step=0x20");
+                    dirFormat = dirFormat.parseChange("/struct=d64-dirent-union");
                 } catch (OptionError e) {
                     throw new RuntimeException("bug detected", e);
                 }
--- src/net/mirky/redis/analysers/StructArrayAnalyser.java
+++ src/net/mirky/redis/analysers/StructArrayAnalyser.java
@@ -8,22 +8,26 @@ import net.mirky.redis.Format;
 import net.mirky.redis.ImageError;
 import net.mirky.redis.Struct;
 
-@Format.Options("array/decoding:decoding=ascii/struct!:struct=unsigned-byte/step!:unsigned-hex=1")
+@Format.Options("array/decoding:decoding=ascii/struct!:struct=unsigned-byte")
 public final class StructArrayAnalyser extends Analyser.Leaf.PossiblyPartial {
     @Override
     protected final int disPartially(Format format, byte[] data, PrintStream port) throws RuntimeException {
         @SuppressWarnings("unchecked")
         Struct struct = ((Format.Option.SimpleOption<Struct>) format.getOption("struct")).value;
-        int step = format.getIntegerOption("step");
         Cursor cursor = new Cursor.ByteArrayCursor(data, 0);
         try {
             int entryNumber = 0; // for the breadcrumb trail
-            while (cursor.probe(step)) {
+            while (!cursor.atEnd()) {
                 if (entryNumber != 0) {
                     port.println();
                 }
-                struct.show(cursor, Integer.toString(entryNumber), port, format.getDecoding());
-                cursor.advance(step);
+                int structSize = struct.show(cursor, Integer.toString(entryNumber), port, format.getDecoding());
+                if (structSize == 0) {
+                    // FIXME: once we output via the {@link ChromaticLineBuilder}, this ought to be shown in red
+                    port.println("!!! null struct");
+                    break;
+                }
+                cursor.advance(structSize);
                 entryNumber++;
             }
         } catch (ImageError e) {
