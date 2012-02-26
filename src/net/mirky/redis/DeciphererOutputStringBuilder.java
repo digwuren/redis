387dd82b57653d32aef9cb8e96f8c29f66f59ede
Andres Soolo <dig@mirky.net>
2012-02-26 15:07:35 +0200

    moved DeciphererOutputStringBuilder into a free class

---
+++ src/net/mirky/redis/DeciphererOutputStringBuilder.java
@@ -0,0 +1,25 @@
+package net.mirky.redis;
+
+final class DeciphererOutputStringBuilder extends DeciphererOutput {
+    public final StringBuilder sb;
+    
+    public DeciphererOutputStringBuilder(StringBuilder sb) {
+        this.sb = sb;
+    }
+
+    public final void append(char c) {
+        sb.append(c);
+    }
+
+    public final void append(String s) {
+        sb.append(s);
+    }
+
+    public final void append(int i) {
+        sb.append(i);
+    }
+
+    public final void switchBack() {
+        // no effect in the output generation phase
+    }
+}
--- src/net/mirky/redis/OutputPhaseDecipherer.java
+++ src/net/mirky/redis/OutputPhaseDecipherer.java
@@ -169,28 +169,4 @@ final class OutputPhaseDecipherer {
             }
         }
     }
-    
-    static final class DeciphererOutputStringBuilder extends DeciphererOutput {
-        public final StringBuilder sb;
-        
-        public DeciphererOutputStringBuilder(StringBuilder sb) {
-            this.sb = sb;
-        }
-
-        public final void append(char c) {
-            sb.append(c);
-        }
-
-        public final void append(String s) {
-            sb.append(s);
-        }
-
-        public final void append(int i) {
-            sb.append(i);
-        }
-
-        public final void switchBack() {
-            // no effect in the output generation phase
-        }
-    }
 }
